9e9c3959be3eb45c71608dd641a34094a77b65b2
charlie-harpe <charlie.augusto@outlook.com>
2022-11-06 11:56:04 -0300

    alterColor

--- NLW_FIGURE/script.js
+++ NLW_FIGURE/script.js
@@ -1,11 +1,7 @@
 let isIgnite = true
-
 function changeCard(event) {
   const card = event.currentTarget
-  const backgroundImage = isIgnite
-    ? 'url(./assets/bg-explorer.svg)'
-    : 'url(./assets/bg-ignite.svg)'
-
-  isIgnite = !isIgnitea
-  card.style.background = 'url(./assets/bg-explorer.svg)'
+  const bg = isIgnite ? 'explorer' : 'ignite'
+  isIgnite = !isIgnite
+  card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
 }
